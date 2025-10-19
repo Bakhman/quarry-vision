15be45e005e70c53e37dd3586a71740f0ab48bd3
Bakhman <begaev7@gmail.com>
2025-10-19 05:24:42 +0300

    fix : release-v1.1.1

--- .github/dist/run.bat
+++ .github/dist/run.bat
@@ -1,8 +1,8 @@
 @echo off
 title QuarryVision
 cd /d "%~dp0"
-set "JAVA=%~dp0jre\bin\javaw.exe"
+set "JAVA=%~dp0jre\bin\java.exe"
 set OPTS=-Xms256m -Xmx1024m -Xss512k -XX:MaxDirectMemorySize=512m ^
  -Dorg.bytedeco.javacpp.maxbytes=4G ^
  -Dorg.bytedeco.javacpp.maxphysicalbytes=4G
-start "" "%JAVA%" %OPTS% -cp "%~dp0config;%~dp0app\quarry-vision.jar" com.quarryvision.app.Boot
+"%JAVA%" %OPTS% -cp "%~dp0config;%~dp0app\quarry-vision.jar" com.quarryvision.app.Boot
